9734f23c5dd8b960be32dbdefd12363b8774f387
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-05-03 16:13:23 -0400

    feat: config tailwind with screens, colors and fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: "375px",
+        desktop: "1440px",
+      },
+      colors: {
+        "neutral-almost-white": "hsl(0, 0%, 98%)",
+        "neutral-medium-gray": "hsl(0, 0%, 41%)",
+        "neutral-almost-black": "hsl(0, 0%, 8%)",
+      },
+      fontFamily: {
+        Epilogue: ["Epilogue"],
+      },
+    },
   },
   plugins: [],
 };
