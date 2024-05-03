c9bda0c3793a9785681118c713d4f31cc9ae9056
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-05-03 15:27:54 -0400

    feat: config vite with project

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+function App() {
+  return (
+    <>
+      <p>Hello World</p>
+
+      {/* Features */}
+      {/* Company */}
+      {/* Careers */}
+      {/* About */}
+
+      {/* Login */}
+      {/* Register */}
+
+      {/* Make remote work */}
+
+      {/* Get your team in sync, no matter your location. Streamline processes,  */}
+      {/* create team rituals, and watch productivity soar. */}
+
+      {/* Learn more */}
+
+      <div className="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </>
+  );
+}
+
+export default App;
