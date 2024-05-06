a9860d52f84f4afb7be024d2f8d9818728bd7e9d
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-05-06 00:05:32 -0400

    style: main feel

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -5,26 +5,34 @@ const { desktopHero, mobileHero } = Images.hero;
 
 export const Main = () => {
   return (
-    <main>
+    <main className="flex flex-col-reverse">
       <section>
-        <h2>Make remote work</h2>
-        <p>
+        <h2 className="text-center text-3xl mb-4 font-bold">
+          Make remote work
+        </h2>
+        <p className="text-center leading-7 text-neutral-medium-gray text-base mx-4 mb-6">
           Get your team in sync, no matter your location. Streamline processes,
           create team rituals, and watch productivity soar.
         </p>
-        <button>Learn more</button>
+        <button className="block bg-neutral-almost-black text-neutral-almost-white w-32 h-12 rounded-2xl mb-[52px] m-auto hover:bg-neutral-almost-white hover:text-neutral-almost-black hover:border-[1px] hover:border-neutral-almost-black">
+          Learn more
+        </button>
 
-        <div>
-          <img src={dataBiz} alt="Logo Client DataBiz" />
-          <img src={audioPhile} alt="Logo Client Audio Phile" />
-          <img src={meet} alt="Logo Client Meet" />
-          <img src={maker} alt="Logo Client Maker" />
+        <div className="flex items-center justify-between gap-1 px-4  mb-24">
+          <img className="w-20" src={dataBiz} alt="Logo Client DataBiz" />
+          <img
+            className="w-[52px]"
+            src={audioPhile}
+            alt="Logo Client Audio Phile"
+          />
+          <img className="w-[64px]" src={meet} alt="Logo Client Meet" />
+          <img className="w-59px" src={maker} alt="Logo Client Maker" />
         </div>
       </section>
       <picture>
         <source srcSet={mobileHero} media="(max-width: 639px)" />
         <source srcSet={desktopHero} media="(max-width: 640px)" />
-        <img src={mobileHero} alt="Imagen Hero" />
+        <img className="w-full mb-12" src={mobileHero} alt="Imagen Hero" />
       </picture>
     </main>
   );
