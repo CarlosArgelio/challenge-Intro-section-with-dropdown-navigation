416f262cd069db6672af5103bceddc2dd877eb06
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-05-05 23:30:28 -0400

    feat: add states to button menu

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Icons } from "./../../assets";
 
 const { logo } = Icons;
@@ -6,28 +7,57 @@ const { down } = Icons.arrow;
 const { calendar, planning, reminders, todoList } = Icons.FeactureSection;
 
 export const Header = () => {
+  const [openMenu, setOpenMenu] = useState<boolean>(false);
+  const [subMenuFeacture, setSubMenuFeacture] = useState<boolean>(false);
+  const [subMenuCompany, setSubMenuCompany] = useState<boolean>(false);
+
+  const handleOpenMenu = () => setOpenMenu(true);
+  const handleCloseMenu = () => setOpenMenu(false);
+
+  const handleSubMenuFeacture = () => {
+    setSubMenuFeacture(!subMenuFeacture);
+  };
+
+  const handleCloseSubMenuCompany = () => {
+    setSubMenuCompany(!subMenuCompany);
+  };
+
   return (
     <header className="flex items-center justify-between m-4 mb-6">
       <img src={logo} alt="Logo SNAP" />
-      <img className="hidden" src={menu} alt="Icon menu view mobile" />
-      <nav className="text-neutral-medium-gray">
+      <button onClick={handleOpenMenu}>
+        <img
+          className={`${openMenu ? "hidden" : ""}`}
+          src={menu}
+          alt="Icon menu view mobile"
+        />
+      </button>
+      <nav className={`text-neutral-medium-gray ${openMenu ? "" : "hidden"}`}>
         {/* hidden */}
         {/* Back Menu */}
         <div className="bg-neutral-medium-gray opacity-75 absolute left-0 top-0 w-full h-[780px]"></div>
         {/* Front Menu */}
         <div className="bg-neutral-almost-white w-60 absolute right-0 top-0 h-[780px] p-5">
           {/* Button X */}
-          <div className="flex items-center justify-end cursor-pointer">
-            <img src={x} alt="" />
+          {/* flex items-center justify-end */}
+          <div className="flex items-center justify-end">
+            <button onClick={handleCloseMenu} className="cursor-pointer">
+              <img src={x} alt="" />
+            </button>
           </div>
           <ul>
             <li className="flex flex-col items-start gap-1 cursor-pointer mb-7">
-              <button className="flex items-center gap-1 cursor-pointer w-24 justify-between">
+              <button
+                onClick={handleSubMenuFeacture}
+                className="flex items-center gap-1 cursor-pointer w-24 justify-between"
+              >
                 Feacture
                 <img src={down} alt="Icon Arrow Down" />
               </button>
 
-              <ul className="relative left-6 hidden">
+              <ul
+                className={`relative left-6 ${subMenuFeacture ? "" : "hidden"}`}
+              >
                 <li className="flex items-center gap-1 cursor-pointer mb-3">
                   <img src={todoList} alt="Icon Todo List" />
                   <span>Todo List</span>
@@ -47,11 +77,16 @@ export const Header = () => {
               </ul>
             </li>
             <li className="flex flex-col items-start gap-1 cursor-pointer mb-7">
-              <button className="flex items-center gap-1 cursor-pointer w-24 justify-between">
+              <button
+                onClick={handleCloseSubMenuCompany}
+                className="flex items-center gap-1 cursor-pointer w-24 justify-between"
+              >
                 Company
                 <img src={down} alt="Icon Arrow Down" />
               </button>
-              <ul className="relative left-6 hidden">
+              <ul
+                className={`relative left-6 ${subMenuCompany ? "" : "hidden"}`}
+              >
                 <li className="cursor-pointer mb-3">History</li>
                 <li className="cursor-pointer mb-3">Our Team</li>
                 <li className="cursor-pointer mb-3">Blog</li>
